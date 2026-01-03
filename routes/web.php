c01600de8a337c509e4f5a7c798b62df1daecd7f
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2026-01-03 09:28:00 +0500

    sdfsadfsadfsafsadf

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+//Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
